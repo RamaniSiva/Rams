d129126dbba590ad750aeb198e54320b5e5f572d
RamaniSiva <sramani710@gmail.com>
2022-03-13 14:49:00 +0530

    welcome

---
+++ src/org/git/GitProgram.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class GitProgram {
+
+	
+	public void add()
+	{
+		int a=10;
+		int b=20;
+				int c=a+b;
+		System.out.println("Print value" +c);
+	}
+	public static void main(String[] args) {
+		
+		GitProgram g=new GitProgram();
+		g.add();
+	}
+
+}
